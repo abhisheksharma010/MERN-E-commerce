a71ab49cabc9d441cbc59f6c30104298f5293938
Abhishek Sharma <abhisheksharma32344@gmail.com>
2023-10-07 20:17:55 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js

--- src/componenets/layout/Header.js
+++ src/componenets/layout/Header.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { NavLink,Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import { useAuth } from "../../context/auth";
 import toast from 'react-hot-toast';
+import SearchInput from "../Forms/SearchInput";
+import useCategory from '../../hooks/useCategory';
+import {useCart} from "../../context/cart"
 
 const Header = () => {
   const [auth, setAuth] = useAuth();
+  const [cart] = useCart();
+  const categories = useCategory();
   const handleLogout = () => {
     setAuth({
       ...auth,
@@ -13,7 +18,7 @@ const Header = () => {
     });
     localStorage.removeItem("auth");
     toast.success("Logout Successfully");
-    
+
     // toast.success("Logout Successfully");
   };
   return (
@@ -26,12 +31,41 @@ const Header = () => {
           <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
             <Link to="/" className="navbar-brand">🛒E-Commerce </Link >
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <SearchInput />
               <li className="nav-item">
-                <NavLink to="/about" className="nav-link" aria-current="page" >Home</NavLink >
+                <NavLink to="/" className="nav-link" aria-current="page" >Home</NavLink >
               </li>
-              <li className="nav-item">
-                <NavLink to="/category" className="nav-link" >category</NavLink >
+
+
+              <li className="nav-item dropdown">
+                <Link
+                  className="nav-link dropdown-toggle"
+                  to={"/categories"}
+                  data-bs-toggle="dropdown"
+                >
+                  Categories
+                </Link>
+                <ul className="dropdown-menu">
+                  <li>
+                    <Link className="dropdown-item" to={"/categories"}>
+                      All Categories
+                    </Link>
+                  </li>
+                  {categories?.map((c) => (
+                    <li>
+                      <Link
+                        className="dropdown-item"
+                        to={`/category/${c.slug}`}
+                      >
+                        {c.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
               </li>
+
+
+
               {!auth?.user ? (
                 <>
                   <li className="nav-item">
@@ -47,16 +81,23 @@ const Header = () => {
                 </>
               ) : (
                 <>
-                <li className="nav-item">
-                    <NavLink onClick={handleLogout} to="/login" className="nav-link">
-                      Logout
+                  <li className="nav-item dropdown">
+                    <NavLink className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      {auth?.user?.name}
                     </NavLink>
+                    <ul className="dropdown-menu">
+                      <li><NavLink to={`/dashboard/${auth?.user?.role === 1 ? "admin" : "user"
+                        }`} className="dropdown-item" >DashBoard</NavLink></li>
+                      <li><NavLink onClick={handleLogout} to="/login" className="dropdown-item" >LogOut</NavLink></li>
+
+                    </ul>
                   </li>
-                      
+
+
                 </>
               )}
               <li className="nav-item">
-                <NavLink to="/cart" className="nav-link">Cart (0)</NavLink >
+                <NavLink to="/cart" className="nav-link">Cart {cart?.length}</NavLink >
               </li>
 
             </ul>
